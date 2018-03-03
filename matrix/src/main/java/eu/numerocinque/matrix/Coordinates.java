09b36da41fa6e824b3b7c9219c13323be96b131e
antoniodecesare <antonio.decesare@gmail.com>
2018-03-03 14:38:10 +0100

    redo Coordinates 1st step

--- matrix/src/main/java/eu/numerocinque/matrix/Coordinates.java
+++ matrix/src/main/java/eu/numerocinque/matrix/Coordinates.java
@@ -1,9 +1,24 @@
 package eu.numerocinque.matrix;
 
 public class Coordinates {
-    private static String string = "string";
+    public static final String RANGEX = "abcdefgh";
+    public static final String RANGEY = "12345678";
 
-    public static String getString() {
-        return string;
+    static int getCoordinatesX(String coordinates) {
+        int x = -1;
+
+        String coordX = coordinates.substring(0, 1).toLowerCase();
+        if (RANGEX.contains(coordX)) x = coordX.charAt(0) - 97;
+
+        return x;
+    }
+
+    static int getCoordinatesY(String coordinates) {
+        int y = -1;
+
+        String coordY = coordinates.substring(1, 2).toLowerCase();
+        if (RANGEY.contains(coordY)) y = coordY.charAt(0) - 49;
+
+        return y;
     }
 }
