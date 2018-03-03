4b3d42a754fff2484f2a600e5abcaf6fddbffef7
antoniodecesare <antonio.decesare@gmail.com>
2018-03-03 15:19:30 +0100

    redo Matrix 1st step

--- matrix/src/main/java/eu/numerocinque/matrix/Matrix.java
+++ matrix/src/main/java/eu/numerocinque/matrix/Matrix.java
@@ -1,5 +1,8 @@
 package eu.numerocinque.matrix;
 
+import static eu.numerocinque.matrix.Coordinates.getCoordinatesX;
+import static eu.numerocinque.matrix.Coordinates.getCoordinatesY;
+
 public class Matrix {
     private final Object[][] matrix;
 
@@ -10,4 +13,27 @@ public class Matrix {
     public Object[][] getMatrix() {
         return matrix;
     }
+
+    private boolean check(int x, int y) {
+        return (x >= 0 && x < matrix.length && y >= 0 && y < matrix[0].length);
+    }
+
+    public void set(String coordinates, Object thisObject) {
+        int x = getCoordinatesX(coordinates);
+        int y = getCoordinatesY(coordinates);
+
+        if (check(x, y)) matrix[x][y] = thisObject;
+    }
+
+    public Object get(String coordinates) {
+        Object thisObject = null;
+
+        int x = getCoordinatesX(coordinates);
+        int y = getCoordinatesY(coordinates);
+
+        if (check(x, y)) thisObject = matrix[x][y];
+
+        return thisObject;
+    }
+
 }
